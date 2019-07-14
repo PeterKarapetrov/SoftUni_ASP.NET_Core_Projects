06379031eda1a1361d4ca7a7cd1573fc09121039
PeterKarapetrov <35077183+PeterKarapetrov@users.noreply.github.com>
2019-07-14 11:39:46 +0300

    Add files via upload
    Initial Project Upload

---
+++ TOPMS/Data/TOPMSContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TOPMS.Models;
+
+namespace TOPMS.Models
+{
+    public class TOPMSContext : DbContext
+    {
+        public TOPMSContext (DbContextOptions<TOPMSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TOPMS.Models.User> Users { get; set; }
+
+        public DbSet<TOPMS.Models.Role> Roles { get; set; }
+
+        public DbSet<TOPMS.Models.UserRole> UserRole { get; set; }
+
+
+    }
+}
---
+++ TOPMS/Models/Role.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TOPMS.Models
+{
+    [Table("Role")]
+    public class Role
+    {
+        [Key, Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Descrition { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
